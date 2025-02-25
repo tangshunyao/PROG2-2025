e2893256d83c023835e87b6fb1fe37f914d643ad
tangshunyao <211449757@qq.com>
2025-02-25 19:18:28 +0800

    Update pizza.java

--- src/pizza.java
+++ src/pizza.java
@@ -1,52 +1,58 @@
 public class pizza {
-    public String storeAddress;
-    public String storeName;
+        public String storeAddress;
+        public String storeName;
+    
+        public String Menu;
+    
+        public long storePhone;
+        public String storEemali;
+        public String pizzaIngredients;
+        public double pizzaPrice;
+        public String pizzaSide;
+        public String drinks;
+    
+        private String orderID;
+        private double orderTotal;
 
-    private String storeMenu;
-
-    public long storePhone;
-    public String storEemali;
-    public String pizzaIngredients;
-    public String pizzaPrice;
-    public String pizzaSide;
-    public String drinks;
-
-    private String orderID;
-    private double orderTotal;
-
-    public void takeorder(String id, double total){
-        orderID = id;
-        orderTotal = total;
-
-        System.out.println("order accepted!");
-
-        System.out.println("order is being prepared");
-
-        try{
-            Thread.sleep(5000);
-        } catch(InterruptedException e){
-            System.out.println("order is ready for pickup!");
+        }
+        public void takeOrder(String id, String ingredients, String sides, String drink, double price, double total){
+            this.orderID = id;
+            this.pizzaIngredients = ingredients;
+            this.pizzaSide = sides;
+            this.drinks = drink;
+            this.pizzaPrice = price;
+            this.orderTotal = total;
+            
+            System.out.println("order accepted!");
+    
+            System.out.println("order is being prepared");
+    
+            try{
+                Thread.sleep(5000);
+            } catch(InterruptedException e){
+                System.out.println("order is ready for pickup!");
+            }
+    
+            System.out.println("your order is ready!");
+             
+            printreceipt();
+        
         }
 
-        System.out.println("your order is ready!");
-         
-        printreceipt();
+        private void printreceipt(){
+            System.out.println("********RECEIPT********");
+    
+            System.out.println("order ID:" + orderID);
+            System.out.println("order total:" + orderTotal);
+        
+        }
+    
+        public void makepizza(){
+            System.out.println("pizza ingredients:"  + pizzaIngredients);
+            System.out.println("pizza sides:"  + pizzaSide);
+            System.out.println("drinks:"  + drinks);
+        } 
     
     }
-
-    private void printreceipt(){
-        System.out.println("order ID:" + orderID);
-        System.out.println("order total:" + orderTotal);
     
-    }
-
-    public void makepizaa(){
-        System.out.println("pizza ingredients:"  + pizzaIngredients);
-        System.out.println("pizza sides:"  + pizzaSide);
-        System.out.println("drinks:"  + drinks);
-    }
-
-    
-
-}
 
